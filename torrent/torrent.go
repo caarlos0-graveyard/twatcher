488e32996f3f7e14e6cd05d7dd846168c0b142f7
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-03-16 22:57:32 -0300

    tvshows start

---
+++ torrent/torrent.go
@@ -0,0 +1,53 @@
+package torrent
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os/user"
+	"strings"
+)
+
+// Torrent is a Title/Href combo
+type Torrent struct {
+	Title string
+	Href  string
+}
+
+// NewTorrent instance
+func NewTorrent(title, href string) *Torrent {
+	return &Torrent{
+		Title: title,
+		Href:  href,
+	}
+}
+
+// Download the torrent to ~/Downloads
+func (t *Torrent) Download() {
+	fmt.Printf("Downloading %s", t.Title)
+	response, err := http.Get(t.Href)
+	if err != nil {
+		fmt.Printf("Failed to download %s", t.Title)
+		return
+	}
+	defer response.Body.Close()
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Printf("Failed to read the contents of %s", t.Title)
+		return
+	}
+
+	if err := ioutil.WriteFile(t.filename(), contents, 0644); err != nil {
+		fmt.Printf("Failed to save the contents of %s to the torrent file", t.Title)
+	}
+}
+
+func (t *Torrent) filename() string {
+	me, err := user.Current()
+	if err != nil {
+		fmt.Printf("Failed to get the current user")
+		return ""
+	}
+	filename := strings.Replace(t.Title, " ", ".", -1)
+	return me.HomeDir + "/Downloads/" + filename + ".torrent"
+}
